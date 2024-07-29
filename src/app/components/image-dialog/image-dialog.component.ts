a4c689edd5d1bf5953a66c9aade6615aceb18821
fgallardo@favric.cl <sadic@MacBook-Pro-de-Felipe.local>
2024-07-28 20:58:18 -0400

    version one

--- src/app/components/image-dialog/image-dialog.component.ts
+++ src/app/components/image-dialog/image-dialog.component.ts
@@ -1,14 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { AngularMaterialModule } from '../../../utils/material.module';
 
 @Component({
   selector: 'app-image-dialog',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AngularMaterialModule],
   templateUrl: './image-dialog.component.html',
   styleUrl: './image-dialog.component.scss'
 })
 export class ImageDialogComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: { images: string[] }) {}
+  constructor(
+    public dialogRef: MatDialogRef<ImageDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { images: string[] }
+    ) {}
+
+  closeDialog(): void {
+    this.dialogRef.close();
+  }
 }
